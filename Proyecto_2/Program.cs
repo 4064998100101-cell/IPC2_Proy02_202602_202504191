678a67fe5582f213ce24b8c6f4b5110007a72999
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-09-02 02:15:06 -0600

    feat: Se agrego la lista libros

--- Proyecto_2/Program.cs
+++ Proyecto_2/Program.cs
@@ -1,1 +1,56 @@
-﻿Console.WriteLine("Carlitos es gay y puto");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Proyecto_2
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            // vamos a crear el menu de inicio para empezar
+            bool repetir=true;
+            int opcion=100;
+            while (repetir)
+            {
+            Console.WriteLine("+===================================+");
+            Console.WriteLine("|        Programa de libreria       |");
+            Console.WriteLine("+===================================+");
+            Console.WriteLine("1. Gestion de libros");
+            Console.WriteLine("2. Lector Xml");
+            Console.WriteLine("0. Salir");
+
+            Console.WriteLine("Ingrese una opcion");
+            try
+            {
+            opcion=int.Parse(Console.ReadLine());
+            
+
+                
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Dato no reconocido");
+
+            }
+                        switch (opcion)
+            {
+                case 1: break;
+                case 2: break;
+                case 0: Console.WriteLine("Gracias por usar el programa de la libreria de datos"); repetir=false; break;
+                
+                default: Console.WriteLine("Opcion no validad intentelo de nuevo....."); break;
+            }
+
+            }
+
+
+        
+
+
+
+        }
+        
+    }
+    
+
+}
---
+++ Proyecto_2/listalibros.cs
@@ -0,0 +1,4 @@
+namespace Proyecto_2
+{
+    
+}
