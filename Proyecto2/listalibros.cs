3da86d0e498a55f65850ae463a79833dd233273f
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-09-20 22:34:18 -0600

    feat: Se termino la lista de los libros

--- Proyecto2/listalibros.cs
+++ Proyecto2/listalibros.cs
@@ -4,6 +4,55 @@ namespace Proyecto2
     {
         public Models.libro Valor{get; private set;}
         public NodoLibro Siguiente {get; private set;}
+
+        public NodoLibro(Models.libro libro)
+        {
+            Valor =libro;
+            Siguiente= null;
+        }
+
+    }
+    public class ListaLibros
+    {
+        public NodoLibro Cabeza {get; private set;}
+        public int Tamaño {get; private set;}
+        public ListaLibros()
+        {
+            Cabeza=null;
+            Tamaño=0;
+        }
+        public void Insertar (Models.Libro nuevoLibro)
+        {
+            NodoLibro nuevoNodo=new NodoLibro(nuevoLibro);
+            if (Cabeza==null)
+            {
+                Cabeza=nuevoLibro;
+            }
+            else
+            {
+                NodoLibro actual=Cabeza;
+                while (actual.Siguiente!= null)
+                {
+                    actual=actual.Siguiente;
+                }
+                actual.Siguiente=nuevoNodo;
+            }
+            Tamaño ++;
+        }
+
+        public Models.libro BuscarISBN(int isbn)
+        {
+            NodoLibro actual=Cabeza;
+            while (actual!= null)
+            {
+                if(actual.Valor.isbn==isbn)
+                {
+                    return actual.Valor;
+                }
+                actual=actual.Siguiente;
+            }
+            return null;
+        }
     }
     
 }
